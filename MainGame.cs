3b1fe18727e88af5116299ba1a5bab1a058e4179
Jack Wren <jack_wr@hotmail.co.uk>
2017-06-15 16:37:53 +0100

    Updated MainGame cs to have Ball positions
    Created some variables to hold the position of the ball, also created skeleton for the user touch method

--- MainGame.cs
+++ MainGame.cs
@@ -26,26 +26,41 @@ namespace ShootingBoots
 
             //Draw Ball
             SetContentView(new MyOvalShape(this));
-
+      
         }
+
+        //public boolean onTouch(View v, MotionEvent event) {
+
+             //Create method to take user touch on screen
+             //Need to find if they have touched the area of the ball
+             //If so increase top and bottom position by 200 etc
+          
+        //}
+
       
     }
 
     public class MyOvalShape : View
     {
         public ShapeDrawable _shape;
+        public int top = 900;
+        public int bottom = 1000;
+        public int right = 450;
+        public int left = 350;
+
 
         public MyOvalShape(Context context) : base(context)
         {
             var paint = new Paint();
-            paint.SetARGB(255, 200, 255, 0);
+            paint.SetARGB(255, 200, 255, 200);
             paint.SetStyle(Paint.Style.Stroke);
             paint.StrokeWidth = 4;
 
             _shape = new ShapeDrawable(new OvalShape());
             _shape.Paint.Set(paint);
 
-            _shape.SetBounds(200, 200, 300, 200);
+            _shape.SetBounds(left, top, right, bottom);
+            
            
         }
 
