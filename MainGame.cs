b6db534ec3a02a47ee700cc80d730944f0815e81
Jack Wren <jack_wr@hotmail.co.uk>
2017-06-21 11:35:59 +0100

    Re factored solution
    Now main game deal with image, and can move that image around screen

--- MainGame.cs
+++ MainGame.cs
@@ -17,96 +17,138 @@ using Android.Graphics.Drawables.Shapes;
 namespace ShootingBoots
 {
     [Activity(Label = "MainGame")]
-    public class MainGame : Activity, GestureDetector.IOnGestureListener
+    public class MainGame : Activity
     {
-        private GestureDetector _gestureDetector;
-        private MyOvalShape _ball;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             //Draw Ball
-            SetContentView(new MyOvalShape(this, 900, 1000));
-            _gestureDetector = new GestureDetector(this);
-            _ball = new MyOvalShape(this, 900, 1000);
+            View _View = new GestureRecognizerView(this);
+            SetContentView(_View);
 
-        }
-
-        public override bool OnTouchEvent(MotionEvent e) {
-
-            _gestureDetector.OnTouchEvent(e);
-            var top_C = _ball.topP - 50;
-            var bottom_C = _ball.bottomP -50;
-
-            _ball = new MyOvalShape(this, top_C, bottom_C);
-            _ball.Invalidate();
-
-            return true;
-          
-        }
-
-        public bool OnDown(MotionEvent e)
-        {
-            return false;
-        }
-
-        public bool OnFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
-        {
-            return true;
-        }
-
-        public void OnLongPress(MotionEvent e)
-        {
-            
-        }
-
-        public bool OnScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
-        {
-            return false;
-        }
-
-        public void OnShowPress(MotionEvent e)
-        {
-            
-        }
-
-        public bool OnSingleTapUp(MotionEvent e)
-        {
-            return false;
         }
 
     }
 
-    public class MyOvalShape : View
+    public class GestureRecognizerView : View
     {
-        public ShapeDrawable _shape;
-        public int topP;
-        public int bottomP;
-        public int right = 450;
-        public int left = 350;
+        public static readonly int InvalidPointerId = -1;
+     
+        public readonly Drawable _icon;
+        public readonly ScaleGestureDetector _scaleDetector;
 
-        public MyOvalShape(Context context, int top, int bottom) : base(context)
+        public int _activePointerId = InvalidPointerId;
+        public float _lastTouchX;
+        public float _lastTouchY;
+        public float _posX;
+        public float _posY;
+        public float _scaleFactor = 1.0f;
+
+        public GestureRecognizerView(Context context) : base(context, null, 0)
         {
-            var paint = new Paint();
-            topP = top;
-            bottomP = bottom;
-            paint.SetARGB(255, 200, 255, 200);
-            paint.SetStyle(Paint.Style.Stroke);
-            paint.StrokeWidth = 4;
-
-            _shape = new ShapeDrawable(new OvalShape());
-            _shape.Paint.Set(paint);
-
-            _shape.SetBounds(left, top, right, bottom);
+            _icon = context.Resources.GetDrawable(Resource.Drawable.Icon);
+            _icon.SetBounds(0, 0, _icon.IntrinsicWidth, _icon.IntrinsicHeight);
+            _scaleDetector = new ScaleGestureDetector(context, new MyScaleListener(this));
 
         }
 
         protected override void OnDraw(Canvas canvas)
         {
-            _shape.Draw(canvas);
+            base.OnDraw(canvas);
+            canvas.Save();
+            canvas.Translate(_posX, _posY);
+            canvas.Scale(_scaleFactor, _scaleFactor);
+            _icon.Draw(canvas);
+            canvas.Restore();
         }
+
+        public override bool OnTouchEvent(MotionEvent ev)
+        {
+            _scaleDetector.OnTouchEvent(ev);
+
+            MotionEventActions action = ev.Action & MotionEventActions.Mask;
+            int pointerIndex;
+
+            switch (action)
+            {
+                case MotionEventActions.Down:
+                    _lastTouchX = ev.GetX();
+                    _lastTouchY = ev.GetY();
+                    _activePointerId = ev.GetPointerId(0);
+                    break;
+
+                case MotionEventActions.Move:
+                    pointerIndex = ev.FindPointerIndex(_activePointerId);
+                    float x = ev.GetX(pointerIndex);
+                    float y = ev.GetY(pointerIndex);
+                    if (!_scaleDetector.IsInProgress)
+                    {
+                        // Only move the ScaleGestureDetector isn't already processing a gesture.
+                        float deltaX = x - _lastTouchX;
+                        float deltaY = y - _lastTouchY;
+                        _posX += deltaX;
+                        _posY += deltaY;
+                        Invalidate();
+                    }
+
+                    _lastTouchX = x;
+                    _lastTouchY = y;
+                    break;
+
+                case MotionEventActions.Up:
+                case MotionEventActions.Cancel:
+                    // We no longer need to keep track of the active pointer.
+                    _activePointerId = InvalidPointerId;
+                    break;
+
+                case MotionEventActions.PointerUp:
+                    // check to make sure that the pointer that went up is for the gesture we're tracking.
+                    pointerIndex = (int)(ev.Action & MotionEventActions.PointerIndexMask) >> (int)MotionEventActions.PointerIndexShift;
+                    int pointerId = ev.GetPointerId(pointerIndex);
+                    if (pointerId == _activePointerId)
+                    {
+                        // This was our active pointer going up. Choose a new
+                        // action pointer and adjust accordingly
+                        int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                        _lastTouchX = ev.GetX(newPointerIndex);
+                        _lastTouchY = ev.GetY(newPointerIndex);
+                        _activePointerId = ev.GetPointerId(newPointerIndex);
+                    }
+                    break;
+
+            }
+            return true;
+        }
+
 
     }
 
+    public class MyScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener
+    {
+        public readonly GestureRecognizerView _view;
+
+        public MyScaleListener(GestureRecognizerView view)
+        {
+            _view = view;
+        }
+
+        public override bool OnScale(ScaleGestureDetector detector)
+        {
+            _view._scaleFactor *= detector.ScaleFactor;
+
+            // put a limit on how small or big the image can get.
+            if (_view._scaleFactor > 5.0f)
+            {
+                _view._scaleFactor = 5.0f;
+            }
+            if (_view._scaleFactor < 0.1f)
+            {
+                _view._scaleFactor = 0.1f;
+            }
+
+            _view.Invalidate();
+            return true;
+        }
+    }
 }
