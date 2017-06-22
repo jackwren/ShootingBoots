40b08e7e84aac780cdf3e2513054a45f39a0cadb
Jack Wren <jack_wr@hotmail.co.uk>
2017-06-22 16:58:17 +0100

    Updated main game method, added Common game lib
    Added new class in separate place to store game functions, so its cross platform.

--- MainGame.cs
+++ MainGame.cs
@@ -1,154 +1,70 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Drawing;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.Graphics;
-using Android.Graphics.Drawables;
-using Android.Graphics.Drawables.Shapes;
+using System;
+using CocosSharp;
+using System.Collections.Generic;
+using ShootingBoots.Common;
+using Android.Content.PM;
 
 namespace ShootingBoots
 {
-    [Activity(Label = "MainGame")]
+    [Activity(Label = "MainGame.Android",
+        MainLauncher = true,
+        Icon = "@drawable/icon",
+        AlwaysRetainTaskState = true,
+        ScreenOrientation = ScreenOrientation.Portrait,
+        LaunchMode = LaunchMode.SingleInstance,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden)]
+
     public class MainGame : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            //Draw Ball
-            View _View = new GestureRecognizerView(this);
-            SetContentView(_View);
+            //Set Game Window
+            SetContentView(Resource.Layout.Game);
+
+            CCGameView gameView = (CCGameView)FindViewById(Resource.Id.GameView);
+            gameView.ViewCreated += LoadGame;
 
         }
-
-    }
-
-    public class GestureRecognizerView : View
-    {
-        public static readonly int InvalidPointerId = -1;
-     
-        public readonly Drawable _icon;
-        public readonly ScaleGestureDetector _scaleDetector;
-
-        public int _activePointerId = InvalidPointerId;
-        public float _lastTouchX;
-        public float _lastTouchY;
-        public float _posX;
-        public float _posY;
-        public float _scaleFactor = 1.0f;
-
-        public GestureRecognizerView(Context context) : base(context, null, 0)
+        void LoadGame(object sender, EventArgs e)
         {
-            _icon = context.Resources.GetDrawable(Resource.Drawable.Icon);
-            _icon.SetBounds(0, 0, _icon.IntrinsicWidth, _icon.IntrinsicHeight);
-            _scaleDetector = new ScaleGestureDetector(context, new MyScaleListener(this));
-
-        }
-
-        protected override void OnDraw(Canvas canvas)
-        {
-            base.OnDraw(canvas);
-            canvas.Save();
-            canvas.Translate(_posX, _posY);
-            canvas.Scale(_scaleFactor, _scaleFactor);
-            _icon.Draw(canvas);
-            canvas.Restore();
-        }
-
-        public override bool OnTouchEvent(MotionEvent ev)
-        {
-            _scaleDetector.OnTouchEvent(ev);
-
-            MotionEventActions action = ev.Action & MotionEventActions.Mask;
-            int pointerIndex;
-
-            switch (action)
+            CCGameView gameView = sender as CCGameView;
+            if (gameView != null)
             {
-                case MotionEventActions.Down:
-                    _lastTouchX = ev.GetX();
-                    _lastTouchY = ev.GetY();
-                    _activePointerId = ev.GetPointerId(0);
-                    break;
+                var contentSearchPaths = new List<string>() { "Fonts", "Sounds" };
+                CCSizeI viewSize = gameView.ViewSize;
 
-                case MotionEventActions.Move:
-                    pointerIndex = ev.FindPointerIndex(_activePointerId);
-                    float x = ev.GetX(pointerIndex);
-                    float y = ev.GetY(pointerIndex);
-                    if (!_scaleDetector.IsInProgress)
-                    {
-                        // Only move the ScaleGestureDetector isn't already processing a gesture.
-                        float deltaX = x - _lastTouchX;
-                        float deltaY = y - _lastTouchY;
-                        _posX += deltaX;
-                        _posY += deltaY;
-                        Invalidate();
-                    }
+                int width = 768;
+                int height = 1027;
 
-                    _lastTouchX = x;
-                    _lastTouchY = y;
-                    break;
+                // Set world dimensions
+                gameView.DesignResolution = new CCSizeI(width, height);
 
-                case MotionEventActions.Up:
-                case MotionEventActions.Cancel:
-                    // We no longer need to keep track of the active pointer.
-                    _activePointerId = InvalidPointerId;
-                    break;
+                // Determine whether to use the high or low def versions of our images
+                // Make sure the default texel to content size ratio is set correctly
+                // Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
+                if (width < viewSize.Width)
+                {
+                    contentSearchPaths.Add("Images/Hd");
+                    CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+                }
+                else
+                {
+                    contentSearchPaths.Add("Images/Ld");
+                    CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+                }
 
-                case MotionEventActions.PointerUp:
-                    // check to make sure that the pointer that went up is for the gesture we're tracking.
-                    pointerIndex = (int)(ev.Action & MotionEventActions.PointerIndexMask) >> (int)MotionEventActions.PointerIndexShift;
-                    int pointerId = ev.GetPointerId(pointerIndex);
-                    if (pointerId == _activePointerId)
-                    {
-                        // This was our active pointer going up. Choose a new
-                        // action pointer and adjust accordingly
-                        int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                        _lastTouchX = ev.GetX(newPointerIndex);
-                        _lastTouchY = ev.GetY(newPointerIndex);
-                        _activePointerId = ev.GetPointerId(newPointerIndex);
-                    }
-                    break;
+                gameView.ContentManager.SearchPaths = contentSearchPaths;
 
-            }
-            return true;
-        }
+                CCScene gameScene = new CCScene(gameView);
 
-
-    }
-
-    public class MyScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener
-    {
-        public readonly GestureRecognizerView _view;
-
-        public MyScaleListener(GestureRecognizerView view)
-        {
-            _view = view;
-        }
-
-        public override bool OnScale(ScaleGestureDetector detector)
-        {
-            _view._scaleFactor *= detector.ScaleFactor;
-
-            // put a limit on how small or big the image can get.
-            if (_view._scaleFactor > 5.0f)
-            {
-                _view._scaleFactor = 5.0f;
-            }
-            if (_view._scaleFactor < 0.1f)
-            {
-                _view._scaleFactor = 0.1f;
+                gameScene.AddLayer(new GameLayer());
+                gameView.RunWithScene(gameScene);
             }
 
-            _view.Invalidate();
-            return true;
         }
     }
 }
