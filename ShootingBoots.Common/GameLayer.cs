21d5d3d1785943f89951874f7af9954ccf08ecbf
jackwren <jack_wr@hotmail.co.uk>
2017-07-04 16:17:23 +0100

    Updated button layout
    Changed the design of home page buttons

--- ShootingBoots.Common/GameLayer.cs
+++ ShootingBoots.Common/GameLayer.cs
@@ -4,7 +4,6 @@ using System;
 using System.Linq;
 using Android.Widget;
 using Android.App;
-using ShootingBoots;
 
 namespace ShootingBoots.Common
 {
--- ShootingBoots/Resources/Resource.Designer.cs
+++ ShootingBoots/Resources/Resource.Designer.cs
@@ -48,10 +48,13 @@ namespace ShootingBoots
 			public const int gameBackground = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
+			public const int home_button = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int mainBackground = 2130837506;
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int mainBackground = 2130837507;
 			
 			static Drawable()
 			{
